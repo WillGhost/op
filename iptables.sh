74029903b250d05928d525784891bb44408b9c0c
gg <gg@.local>
2021-08-19 14:11:45 +0800

    up

--- iptables.sh
+++ iptables.sh
@@ -4,11 +4,10 @@ iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
-iptables -A INPUT -p tcp --dport 8900 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
 iptables -A INPUT -p tcp --dport 3724 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -s 172.24.5.0/24  -j ACCEPT
+iptables -A INPUT -s 172.24.5.0/24 -j ACCEPT
 iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 
 
@@ -19,8 +18,7 @@ ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 8000 -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 8900 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 3724 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
 ip6tables -A INPUT -j REJECT
