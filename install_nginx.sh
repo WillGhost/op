63b8bb1f1bf145c6b08bd0861a0010711991ae63
gg <gg@.local>
2021-08-21 16:54:12 +0800

    nginx

--- install_nginx.sh
+++ install_nginx.sh
@@ -18,6 +18,9 @@ cd nginx-$version
 --with-http_ssl_module \
 --with-http_stub_status_module \
 --with-http_realip_module \
+--with-stream \
+--with-stream_ssl_module \
+--with-stream_ssl_preread_module \
 --with-http_gzip_static_module \
 --with-http_v2_module > /dev/null \
 && make > /dev/null && make install > /dev/null
