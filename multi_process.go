3939ad6ac7d74cd1d3305026d4d0fcce95fd460a
gg <gg@.local>
2021-08-23 16:06:31 +0800

    ww

--- multi_process.go
+++ multi_process.go
@@ -21,9 +21,10 @@ func DoWork(cc chan int) {
 }
 
 func main() {
-	cc := make(chan int, 10)
+	Concurrent := 3
+	cc := make(chan int)
 
-	for i := 0; i < 3; i++ {
+	for i := 0; i < Concurrent; i++ {
 		wg.Add(1)
 		go DoWork(cc)
 	}
