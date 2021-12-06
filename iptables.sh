1a501863e6e0fef52493ec32e70ea5c6db21d46f
GG <sdbc1178@gmail.com>
2021-12-07 01:45:20 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -17,11 +17,11 @@ ip6tables -F INPUT
 ip6tables -I INPUT -i lo -j ACCEPT
 ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 3724 -j ACCEPT
-ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
+#ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
+#ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
+#ip6tables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
+#ip6tables -A INPUT -p tcp --dport 3724 -j ACCEPT
+#ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
 ip6tables -A INPUT -j REJECT
 
 
