4606c77cf6615b8b7e37e14a8ff8a8b09795c57b
gg <gg@.local>
2023-02-15 00:14:08 +0800

    nginx trans

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -4,15 +4,17 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export TZ=Asia/Shanghai
 
-sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
+#sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 
 apt update \
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone \
+#ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone \
 apt install -y tzdata
 
 
 apt install -y lrzsz tree vim dnsutils zip unzip wget curl git telnet sysstat tar less iputils-ping iproute2 \
-  python3 python3-dev python3-pip
+  python3 python3-dev python3-pip \
+  mysql-client libmysqlclient-dev \
+  screen tmux
 
 curl -s -o /tmp/vimrc https://cdn.jsdelivr.net/gh/WillGhost/op/vimrc && \
   grep -n =========== /tmp/vimrc |awk -F: '{print $1}' |xargs -I {} sed -n '{},1000p' /tmp/vimrc > ~/.vimrc && \
