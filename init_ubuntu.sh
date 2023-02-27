b753de14a118ac9e3db8b56f48448853e692e0e1
gg <gg@.local>
2023-02-27 22:25:53 +0800

    up

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -2,19 +2,16 @@
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-export TZ=Asia/Shanghai
 
 #sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 
-apt update \
-#ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone \
+apt update 
 apt install -y tzdata
+ln -snf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo 'Asia/Shanghai' > /etc/timezone
 
 
-apt install -y lrzsz tree vim dnsutils zip unzip wget curl git telnet sysstat tar less iputils-ping iproute2 \
-  python3 python3-dev python3-pip \
-  mysql-client libmysqlclient-dev \
-  screen tmux
+apt install -y lrzsz tree vim dnsutils zip unzip wget curl git telnet sysstat tar less iputils-ping iproute2 screen tmux jq
+#apt install -y  python3 python3-dev python3-pip mysql-client libmysqlclient-dev 
 
 curl -s -o /tmp/vimrc https://cdn.jsdelivr.net/gh/WillGhost/op/vimrc && \
   grep -n =========== /tmp/vimrc |awk -F: '{print $1}' |xargs -I {} sed -n '{},1000p' /tmp/vimrc > ~/.vimrc && \
@@ -23,16 +20,18 @@ curl -s -o /tmp/vimrc https://cdn.jsdelivr.net/gh/WillGhost/op/vimrc && \
 curl -s -o /tmp/bashrc https://cdn.jsdelivr.net/gh/WillGhost/op/bashrc && \
   cat ~/.bashrc |grep history-search-backward || cat /tmp/bashrc >> ~/.bashrc
 
-#RUN wget -q https://gomirrors.org/dl/go/go1.16.3.linux-amd64.tar.gz && \
-#  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz && \
-#  rm -f go1.16.3.linux-amd64.tar.gz
-#ENV PATH=$PATH:/usr/local/go/bin
 
 mkdir /root/.pip/ && echo '[global]\nindex-url = https://mirrors.aliyun.com/pypi/simple' > ~/.pip/pip.conf
 #pip3 install ipython && \
 
-ls /etc/rc.local || echo '#!/usr/bin/bash' > /etc/rc.local
+ls /etc/rc.local || touch /etc/rc.local
 chmod +x /etc/rc.local
 systemctl enable rc-local
 
+curl -L -o /usr/local/sbin/nexttrace https://github.com/sjlleo/nexttrace/releases/download/v1.1.2/nexttrace_linux_amd64
+chmod +x /usr/local/sbin/nexttrace 
 
+#RUN wget -q https://gomirrors.org/dl/go/go1.16.3.linux-amd64.tar.gz && \
+#  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz && \
+#  rm -f go1.16.3.linux-amd64.tar.gz
+#ENV PATH=$PATH:/usr/local/go/bin
