07386bd1c2df3dd673391dd183882d3ceb08eb94
gg <gg@.local>
2023-03-02 18:34:58 +0800

    iptables

--- iptables.sh
+++ iptables.sh
@@ -1,27 +1,29 @@
 #!/usr/bin/bash
 
 iptables -F
-iptables -I INPUT -i lo -j ACCEPT
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
 iptables -A INPUT -p tcp --dport 3724 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 
 
 
 ip6tables -F INPUT
-ip6tables -I INPUT -i lo -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 3724 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
+ip6tables -A INPUT -i lo -j ACCEPT
 ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
-#ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
-#ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
-#ip6tables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
-#ip6tables -A INPUT -p tcp --dport 3724 -j ACCEPT
-#ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
 ip6tables -A INPUT -j REJECT
 
+
+iptables -F -t nat
 iptables -t nat -A PREROUTING  -p udp --dport 40000:50000 -j DNAT --to-destination :443
