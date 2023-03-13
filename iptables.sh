60ca6b50d55c98234a2595a0dffdd239170191a3
gg <gg@.local>
2023-03-13 12:52:17 +0800

    iptables

--- iptables.sh
+++ iptables.sh
@@ -4,11 +4,15 @@ iptables -F
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -p tcp --dport 3724 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --dport 68 -j ACCEPT
+iptables -A INPUT -p udp --dport 546 -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 
 
@@ -17,13 +21,26 @@ ip6tables -F INPUT
 ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
+ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 3724 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
+ip6tables -A INPUT -p udp --dport 53 -j ACCEPT
+ip6tables -A INPUT -p tcp --dport 53 -j ACCEPT
+ip6tables -A INPUT -p udp --dport 68 -j ACCEPT
+ip6tables -A INPUT -p udp --dport 546 -j ACCEPT
 ip6tables -A INPUT -i lo -j ACCEPT
 ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
 ip6tables -A INPUT -j REJECT
 
 
 iptables -F -t nat
 iptables -t nat -A PREROUTING  -p udp --dport 40000:50000 -j DNAT --to-destination :443
+
+
+in_port=3750
+out_ip=201.18.104.105
+out_port=22
+my_ip=103.233.0.9
+iptables -t nat -A PREROUTING -p tcp --dport $in_port -j DNAT --to-destination $out_ip:$out_port
+iptables -t nat -A POSTROUTING -d $out_ip -p tcp --dport $out_port -j SNAT --to-source $my_ip
+
