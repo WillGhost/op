12c6bf1d6c084569751f8979a72a1526fcd3cd27
GG <sdbc1178@gmail.com>
2023-07-24 16:32:40 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -4,6 +4,7 @@ iptables -F
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
+iptables -A INPUT -p udp --dport 20000:50000 -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -p tcp --dport 3724 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
@@ -21,6 +22,7 @@ ip6tables -F
 ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 443 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 8000:9000 -j ACCEPT
+ip6tables -A INPUT -p udp --dport 20000:50000 -j ACCEPT
 ip6tables -A INPUT -p ipv6-icmp -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 3724 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
@@ -37,11 +39,11 @@ iptables -F -t nat
 iptables -t nat -A PREROUTING  -p udp --dport 40000:50000 -j DNAT --to-destination :443
 
 
-my_ip=103.233.0.9
+#my_ip=103.233.0.9
 
-in_port=3750
-out_ip=201.18.104.105
-out_port=22
-iptables -t nat -A PREROUTING -p tcp --dport $in_port -j DNAT --to-destination $out_ip:$out_port
-iptables -t nat -A POSTROUTING -d $out_ip -p tcp --dport $out_port -j SNAT --to-source $my_ip
+#in_port=3750
+#out_ip=201.18.104.105
+#out_port=22
+#iptables -t nat -A PREROUTING -p tcp --dport $in_port -j DNAT --to-destination $out_ip:$out_port
+#iptables -t nat -A POSTROUTING -d $out_ip -p tcp --dport $out_port -j SNAT --to-source $my_ip
 
