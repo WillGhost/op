e5a8a8822720d8d2e995196e1eef4313fe93117e
Your Name <you@example.com>
2023-12-15 16:35:51 +0800

    ddns

--- ddns.py
+++ ddns.py
@@ -1,49 +1,69 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import socket
 import requests
 
 # https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
 
-key = 'sUZHID'
-zone = '4653b6b34'
-name = '6pi.xxxxxx.com'
-
+key = 'sUZF4G2B87YHID'
+zone = '46d2353b6b34'
+name = '6v.w3r32.com'
 
 url = "https://api.cloudflare.com/client/v4"
+
+
+myipv6url = 'http://v6.ipv6-test.com/api/myip.php'
+#myipv6url = 'https://6.ipw.cn'
 
 headers = {
     "Authorization": "Bearer " + key,
 }
 
+def get_myipv6():
+    ipv6_address = ''
+    try:
+        ipv6_addresses = socket.getaddrinfo(name, None, socket.AF_INET6)
+        for addr_info in ipv6_addresses:
+           ipv6_address = addr_info[4][0]
+    except socket.gaierror:
+        print("Could not resolve the domain namexxxxxxx")
+    return ipv6_address
 
-uri = f'{url}/zones/{zone}/dns_records'
-dd = {
-    'name': name,
-    'type': 'AAAA',
-}
 
-r = requests.get(uri, headers=headers, params=dd)
-print(r.json())
+def get_from_dns():
+    uri = f'{url}/zones/{zone}/dns_records'
+    dd = {
+        'name': name,
+        'type': 'AAAA',
+    }
+    r = requests.get(uri, headers=headers, params=dd)
+    gg = r.json()
+    identifier = gg['result'][0]['id']
+    old =  gg['result'][0]['content']
+    return old, identifier
 
+def get_current():
+    myip = requests.get(myipv6url, timeout=4).text
+    return myip
 
-gg = r.json()
-identifier = gg['result'][0]['id']
-old =  gg['result'][0]['content']
-
-# 6.ipw.cn
-myip = requests.get('http://v6.ipv6-test.com/api/myip.php').text
-print('--------myip', myip)
-
-if old != myip:
+def flush_dns(new_ip, identifier):
     uri = f'{url}/zones/{zone}/dns_records/{identifier}'
     dd = {
         'type': 'AAAA',
-        'content': myip,
+        'content': new_ip,
         'name': name,
     }
     r = requests.put(uri, headers=headers, json=dd)
     print(r.json())
 
+def main():
+    myipv6, identifier = get_from_dns()
+    current_ip = get_current()
+    if myipv6 != current_ip:
+        flush_dns(current_ip, identifier)
 
+
+if __name__ == '__main__':
+    main()
 
 
 
